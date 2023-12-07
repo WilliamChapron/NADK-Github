0c0640166157e198453d2e90afb2674085bf5d7e
William <williamchapronpro@gmail.com>
2023-12-07 12:42:16 +0100

    sdds
    sdsd

--- src/NPC.js
+++ src/NPC.js
@@ -1,18 +1,39 @@
 class NPCManager {
     constructor(name) {
       this.name = name;
-      this.dialogues = [];
-      this.actions = [];
+      this.npcs = [];
     }
   
-    addDialogue(dialogue) {
-      this.dialogues.push(dialogue);
+    // Ajouter un nouveau NPC avec ses dialogues et actions
+    addNPC(dialogues, actions) {
+      const newNPC = {
+        dialogues: dialogues,
+        actions: actions,
+      };
+      this.npcs.push(newNPC);
     }
   
-    addAction(action) {
-      this.actions.push(action);
+    // Obtenir le NPC actuel
+    getCurrentNPC(index) {
+      return this.npcs[index];
     }
   
-
-}
+    // Ajouter des dialogues à un NPC existant
+    addDialoguesToNPC(index, dialogues) {
+      this.npcs[index].dialogues.push(...dialogues);
+    }
+  
+    // Ajouter des actions à un NPC existant
+    addActionsToNPC(index, actions) {
+      this.npcs[index].actions.push(...actions);
+    }
+  }
+  
+  // Exemple d'utilisation :
+  const npcManager = new NPCManager("NPC Manager");
+  
+  // Ajouter plusieurs NPCs avec leurs dialogues et actions
+  npcManager.addNPC(["Dialogue 1", "Dialogue 2"], ["Action 1", "Action 2"]);
+  npcManager.addNPC(["Dialogue 3", "Dialogue 4"], ["Action 3", "Action 4"]);
+  // Ajouter plus de NPCs au besoin
   
